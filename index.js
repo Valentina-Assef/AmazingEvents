5443ae8a87ac10a86a4d4415f927b56cdd3301dc
Valentina Assef <valenassef@gmail.com>
2023-03-07 02:45:54 -0300

    Declaracion de funciones

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 import data from "./amazing.js"
+
+function agregarTodos(array) {
+    for(const evento of data.events) {
+        const card = `<div class="card card-index">
+        <img src=${evento.image} class="card-img-top" alt=""/>
+          <div class="card-body">
+            <h5 class="card-title text-center">${evento.name}</h5>
+            <h6 class="card-date text-center">${evento.date}</h6>
+            <p class="card-text text-center">${evento.description}</p>
+          </div>
+          <div class="footer-card d-flex">
+            <P>Precio $${evento.price}</P>
+            <a href="./details.html" class="btn btn-see-more">Ver mas...</a>
+          </div>
+        </div>`
+        const cardSection = document.getElementById("cardSection")
+        cardSection.innerHTML += card
+    }
+}
+
+agregarTodos(data.events);
